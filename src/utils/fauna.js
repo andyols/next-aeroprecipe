90165cc40cbb04cbe540f50c0250a8644af32333
andyols <andyols.dev@gmail.com>
2021-01-12 15:03:43 -0600

    getRecipes fauna query

--- src/utils/fauna.js
+++ src/utils/fauna.js
@@ -3,7 +3,19 @@ const secret = process.env.FAUNA_SECRET
 const faunaClient = new faunadb.Client({ secret })
 const q = faunadb.query
 
-const getRecipes = async () => {}
+const getRecipes = async () => {
+  const { data } = await faunaClient.query(
+    q.Map(
+      q.Paginate(q.Documents(q.Collection('recipes'))),
+      q.Lambda('ref', q.Get(q.Var('ref')))
+    )
+  )
+  return data.map(recipe => {
+    recipe.id = recipe.ref.id
+    delete recipe.ref
+    return recipe
+  })
+}
 const getRecipeById = async () => {}
 const createRecipe = async () => {}
 const updateRecipe = async () => {}
