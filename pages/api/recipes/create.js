90999b838bc00a6ae06c144ac85ac7a1e041cd92
andyols <andyols.dev@gmail.com>
2021-01-13 18:25:15 -0600

    combined majority of db code into next api

--- pages/api/recipes/create.js
+++ pages/api/recipes/create.js
@@ -1,14 +1,40 @@
-import { createRecipe } from '@/utils/fauna'
+import { CREATE_RECIPE } from '@/utils/graphql'
+import { sendFaunaQuery } from '@/utils/functions'
 
 export default async (req, res) => {
   if (req.method !== 'POST')
     return res.status(405).json({ msg: 'Method not allowed' })
 
+  const {
+    title,
+    creator,
+    method,
+    coffee,
+    grind,
+    water,
+    temperature,
+    time,
+  } = req.body
+
   try {
-    const newRecipe = await createRecipe(req.body)
-    return res.status(200).json(newRecipe)
+    const { createRecipe: createdRecipe } = await sendFaunaQuery(
+      CREATE_RECIPE,
+      {
+        title,
+        creator,
+        method,
+        coffee,
+        grind,
+        water,
+        temperature,
+        time,
+      }
+    )
+    return res.status(200).json(createdRecipe)
   } catch (err) {
     console.error(err)
-    return res.status(500).json({ msg: 'Something went wrong!' })
+    return customResponse(500, {
+      msg: 'Something went wrong...',
+    })
   }
 }
--- pages/api/recipes/get.js
+++ pages/api/recipes/get.js
@@ -1,14 +1,15 @@
-import { getRecipes } from '@/utils/fauna'
+import { GET_RECIPES } from '@/utils/graphql'
+import { sendFaunaQuery } from '@/utils/functions'
 
 export default async (req, res) => {
   if (req.method !== 'GET')
     return res.status(405).json({ msg: 'Method not allowed' })
 
   try {
-    const { status, body } = await getRecipes()
-    return res.status(status).json(body)
+    const { allRecipes } = await sendFaunaQuery(GET_RECIPES)
+    return res.status(200).json(allRecipes.data)
   } catch (err) {
     console.error(err)
-    return res.status(500).json({ msg: 'Something went wrong' })
+    return res.status(200).json({ msg: 'Something went wrong' })
   }
 }
