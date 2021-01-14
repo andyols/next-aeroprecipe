5deee3a5f3b3ee29a320fb9c26d95ae6a8d3b58b
andyols <andyols.dev@gmail.com>
2021-01-14 15:35:19 -0600

    move yup form schema to own module

--- components/Recipes/Form.jsx
+++ components/Recipes/Form.jsx
@@ -3,37 +3,11 @@ import { FormInput, FormNumberInput, FormSelect } from '.'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
-import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useMutation } from 'react-query'
 import { useQueryClient } from 'react-query'
 import { createRecipe } from '@/utils/axios'
-
-const schema = yup.object().shape({
-  title: yup
-    .string()
-    .max(64, 'This title is too powerful!')
-    .required('The recipe needs a name!'),
-  creator: yup
-    .string()
-    .max(64, "That's a long name! Maybe a little too long..."),
-  coffee: yup
-    .number()
-    .min(5, "I think we're gonna need more than that!")
-    .max(50, 'Easy there, big shot!'),
-  water: yup
-    .number()
-    .min(20, 'Not sure how that will be possible...')
-    .max(300, 'What kind of Aeropress do you have?!'),
-  temperature: yup
-    .number()
-    .min(20, 'You think an ice cube is gonna brew very well?')
-    .max(100, 'Are you trying to use a pressure cooker?!'),
-  time: yup
-    .number()
-    .min(10, 'Do you have a time machine or something?')
-    .max(1800, 'Jeez, you think anyone is gonna wait that long for coffee?'),
-})
+import { schema } from '@/lib/schema'
 
 const RecipeForm = () => {
   const router = useRouter()
