d45c11cffe7e856daa0b9960d7d516b7fa7a5806
andyols <andyols.dev@gmail.com>
2021-01-17 10:38:34 -0600

    fix link

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -22,7 +22,7 @@ const Header = props => {
       </Link>
       <Spacer />
       <Flex align='center'>
-        <Link href='/recipe/create'>
+        <Link href='/recipe/create/information'>
           <Button
             colorScheme='teal'
             size='sm'
